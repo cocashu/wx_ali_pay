9aa96764816e28682259a912603257086bb06aee
cocashu <cocashu@gmail.com>
2022-03-05 17:15:45 +0800

    Update 计算程序运行时间毫秒级.py

--- 计算程序运行时间毫秒级.py
+++ 计算程序运行时间毫秒级.py
@@ -1,9 +1,8 @@
 import time
 
-import datetime
-
 ss=int(round(time.time() * 1000))    #毫秒级时间戳
-print(ss)
+#print(ss)
+#需要计时的过程代码
 for num in range(10,5000):  # 迭代 10 到 20 之间的数字
    for i in range(2,num): # 根据因子迭代
       if num%i == 0:      # 确定第一个因子
@@ -13,7 +12,6 @@ for num in range(10,5000):  # 迭代 10 到 20 之间的数字
    else:                  # 循环的 else 部分
       print ('%d 是一个质数' % num)
 en=int(round(time.time() * 1000))    #毫秒级时间戳
-print(en)
+#print(en)
 
-ok=en-ss
-print('过程耗时：',en-ss)
+print('过程耗时：', en - ss, '毫秒')
